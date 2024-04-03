Fix non-determinism caused by usage of random and fix the pyramid noise scaling

library/custom_train_functions.py
@@ -2,6 +2,7 @@ import torch
 import argparse
 import random
 import re
+import numpy as np
 from typing import List, Optional, Union
 from .utils import setup_logging
 
@@ -454,12 +455,13 @@ def pyramid_noise_like(noise, device, iterations=6, discount=0.4):
     b, c, w, h = noise.shape  # EDIT: w and h get over-written, rename for a different variant!
     u = torch.nn.Upsample(size=(w, h), mode="bilinear").to(device)
     for i in range(iterations):
-        r = random.random() * 2 + 2  # Rather than always going 2x,
+        r = np.random.uniform(2, 4) # Rather than always going 2x,
         wn, hn = max(1, int(w / (r**i))), max(1, int(h / (r**i)))
         noise += u(torch.randn(b, c, wn, hn).to(device)) * discount**i
         if wn == 1 or hn == 1:
             break  # Lowest resolution is 1x1
-    return noise / noise.std()  # Scaled back to roughly unit variance
+    # return noise / noise.std()  # Scaled back to roughly unit variance
+    return noise / noise.std(dim=(1, 2,3)).view(b, 1, 1, 1)
 
 
 # https://www.crosslabs.org//blog/diffusion-with-offset-noise